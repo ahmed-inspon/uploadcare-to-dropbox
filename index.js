88bfe9879c3ca94d9af75c4252af0f4d684fbcb0
ahmed inspon <ahmed.quereshi@inspon.com>
2023-10-16 14:37:19 +0500

    cron added

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const bodyparser = require('body-parser');
 var Dropbox = require('dropbox').Dropbox;
 const axios = require('axios');
-const {writeFileSync,readFileSync,unlinkSync} = require('fs');
+const {writeFileSync,readFileSync,unlinkSync, existsSync} = require('fs');
 const { join } = require('path');
 const dotenv = require('dotenv');
 const FormData = require('form-data');
+const sqlite3 = require('sqlite3').verbose();
+const {CronJob} = require('cron');
+let db = new sqlite3.Database('./file.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to database.');
+});
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, name TEXT ,url TEXT, created_at TEXT)");
+});
+
+
+
 dotenv.config();
 const app = express();
 app.use(bodyparser.json());
@@ -67,34 +81,97 @@ let sample = {
   }
 app.post('/webhook',async(req,res)=>{
     let {data,file} = req.body;
-    // console.log("payload---->",data,'<----payload')
-    const fileResponse = await axios({
-        url: file,
-        method: "GET",
-        responseType: "arraybuffer",
-    });
     let file_uuid = data.uuid;
     let ext = data.original_filename.split('.')[data.original_filename.split(".").length-1];
     let file_name = file_uuid+"."+ext;
-    writeFileSync(join(process.cwd(),'temp',file_name),Buffer.from(fileResponse.data),{encoding:'binary'});
-    let file_content = readFileSync(join(process.cwd(),'temp',file_name));
-    let today = new Date();
-    let path = today.getFullYear()+"/"+(today.getMonth()+1);
-    var dbx = new Dropbox({ accessToken: await get_refresh_token()});
-    dbx.filesUpload({path: "/"+path+"/"+file_name, contents: file_content}).then((res)=>{
-        console.log("File Upload at",new Date())
-        try{
-          unlinkSync(join(process.cwd(),'temp',file_name));
-        }
-        catch(err){
-          console.log('err',err);
-        }
-      }).catch((err)=>{
-        console.log("dropbox err",err);
-    })
+    db.run('INSERT INTO files (name,url,created_at) VALUES (?,?,?)', [file_name,file,Date.now()], (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('File name inserted into the database:', file_name);
+      }
+    });
     res.json({success:true,data})
 })
 
 app.listen(7000,()=>{
     console.log("server started");    
 })
+let cron_running = false;
+const cronExecution = () =>{
+  try {
+    if(cron_running)
+    {
+      console.warn('existing cron is not completed');
+      return;
+    }
+  
+    cron_running = true;
+    db.all('SELECT id,name,url,created_at FROM files LIMIT 100', async (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        cron_running = false;
+      } else {
+          if(!rows.length){            
+            cron_running = false;
+            return;
+          }
+          console.log("Found ",rows.length," files for cronjob");
+          let dbx = new Dropbox({ accessToken: await get_refresh_token()});
+          for(let i=0;i < rows.length ; i++){
+            const row = rows[i];
+            const file_name = row.name;
+            const url = row.url;
+            console.log('File Name:', file_name,url);
+            if(!existsSync(join(process.cwd(),'temp',file_name))){
+              try {
+                const fileResponse = await axios({
+                  url: url,
+                  method: "GET",
+                  responseType: "arraybuffer",
+                });
+                writeFileSync(join(process.cwd(),'temp',file_name),Buffer.from(fileResponse.data),{encoding:'binary'});
+              } catch (error) {
+                console.error("file does not exist")
+                continue;
+              }
+            }
+            let file_content = readFileSync(join(process.cwd(),'temp',file_name));
+            let today = new Date();
+            let path = today.getFullYear()+"/"+(today.getMonth()+1);
+            await new Promise((res,rej)=>{
+              dbx.filesUpload({path: "/"+path+"/"+file_name, contents: file_content}).then((res)=>{
+                console.log(file_name," File uploaded at",new Date())
+                try{
+                  unlinkSync(join(process.cwd(),'temp',file_name));
+                  db.run('DELETE FROM files WHERE id = ?',[row.id],()=>{
+                    console.log(file_name," File deleted at",new Date())
+                  })
+                  res()
+                }
+                catch(err){
+                  console.log('err',err,file_name);
+                  res()
+                }
+              }).catch((err)=>{
+                console.log("dropbox err",err);
+                res()
+              })
+            })
+          }
+          cron_running = false;
+      }
+    });
+  } catch (error) {
+    console.error('error happened in cron')
+    cron_running = false;
+  }
+
+}
+const job = new CronJob(
+	'*/2 * * * *',
+	cronExecution,
+	null,
+	true,
+);
+job.start()
