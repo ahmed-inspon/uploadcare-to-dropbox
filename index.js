920154314e1c2a03a654e0c6194c115d7ae43bf2
ahmed inspon <ahmed.quereshi@inspon.com>
2023-10-17 14:36:43 +0500

    .

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ function upload_big_files(file_name, id) {
 
     fileStream.on('data', (chunk) => {
       if (!sessionId) {
-        console.log("chunk size",chunk.size,offset);
+        console.log("chunk size",chunk,offset);
         // Start a new upload session
         dbx.filesUploadSessionStart({ contents: chunk })
           .then((response) => {
